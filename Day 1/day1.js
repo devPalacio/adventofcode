a01c25aada1464f9ee0c904ec85aff38aa4605f7
Jay Palacio <jaypalacio@gmail.com>
2021-12-01 21:27:24 -0500

    part 2 complete

--- Day 1/day1.js
+++ Day 1/day1.js
@@ -11,12 +11,26 @@ console.log("first: %d, last: %d\n", arr[0], arr[arr.length - 1]);
 // ex: 2 - 1 = 1 (increase)
 // ex 2 - 3 = -1 (decrease)
 let increase = 0
-console.log(arr.length)
+
 for (let i = 0; i < arr.length; i++) {
 	if (arr[(i + 1)] - arr[i] > 0) {
 		increase++;
-		console.log(arr[i])
 	}
 }
-console.log(increase)
-return increase;
+console.log(increase + " measurements increased")
+
+
+//part 2 - compare a 3 measurement sliding window
+//i 0 + 1 + 2 vs i 1 + 2 + 3
+let increase3 = 0
+for (let i = 0; i < arr.length; i++) {
+	if (i + 1 > arr.length) { break }
+	let window1 = (arr[i] + arr[i + 1] + arr[i + 2]);
+	let window2 = (arr[i + 1] + arr[i + 2] + arr[i + 3]);
+	if (window2 - window1 > 0) {
+		console.log(window2, window1, increase3)
+		increase3++;
+	}
+}
+console.log(increase3 + " 3 window increase")
+return increase3;
